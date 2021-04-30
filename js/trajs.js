d16b6e536912d24599af08090c8d62f9795167e5
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2021-04-30 14:59:50 +0530

    Add files via upload

---
+++ js/trajs.js
@@ -0,0 +1,20 @@
+document.getElementById("defaultOpen").click();
+function opentab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabs");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tabnumber");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" curr", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += "active";
+  }
